8fc70b8627ca24251529bbd6ec886084c5819fdf
cchiu727 <61135705+cchiu727@users.noreply.github.com>
2024-12-07 16:22:59 -0800

    Add HeroTemplate component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,23 @@
-// import localFont from "next/font/local";
-import "./globals.css";
+import { Rubik } from 'next/font/google';
+import './globals.css';
 
 import Navbar from '@/components/navbar/Navbar';
 // import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 
+const rubik = Rubik({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export const metadata = {
-  title: "Sudz Laundry House",
-  description: "Premiere 5 Star Laundry Services",
+  title: 'Sudz Laundry House',
+  description: 'Premiere 5 Star Laundry Services',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={rubik.className}>
       <body>
         <div className="flex-layout">
           <Navbar />
---
+++ src/components/main/HeroTemplate.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from '@/components/main/HeroTemplate.module.css';
+import classNames from 'classnames';
+
+export default function HeroTemplate({ imageURL, headerText, subtitleText, coaURL, coaButtonText, coaSubtitleText}) {
+    return (
+        <div className={/* classNames(styles.heroContainer, 'container') */ styles.heroContainer}>
+            <div className={styles.heroImageContainer}>
+                {/* <Image src={ '/bubbles.svg' } alt="Background bubbles graphic" width="200" height="200" unoptimized className={styles.heroBackgroundImage} /> */}
+                <Image src={ imageURL } alt="Placeholder" width="360" height="240" unoptimized className={styles.heroImage} />
+            </div>
+            <div className={classNames(styles.heroContentContainer, 'container', 'subcontainer')}>
+                <h1 className={styles.heroHeader}>{headerText}</h1>
+                <p className='subtitle-md'>{subtitleText}</p>
+                <div className={styles.coaContainer}>
+                    <Link href={coaURL}><button className={styles.coaButton}>{coaButtonText}</button></Link>
+                    <p><em>{coaSubtitleText}</em></p>
+                </div>
+            </div>
+        </div>
+    );
+}
